1ec3f062da1b4aa3dafad4850da3698bdda929d6
kinaer <kinaer@naver.com>
2017-01-06 12:51:36 +0900

    말풍선

--- DayToon/app/src/main/java/com/project/aek/daytoon/AddBalloonActivity.java
+++ DayToon/app/src/main/java/com/project/aek/daytoon/AddBalloonActivity.java
@@ -1,20 +1,34 @@
 package com.project.aek.daytoon;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
+import com.project.aek.daytoon.widget.EditBalloonView;
+import com.project.aek.daytoon.widget.TextBalloon;
+
 import java.io.File;
+import java.util.ArrayList;
 
 /**
  * Created by aek on 2016-12-31.
@@ -22,26 +36,51 @@ import java.io.File;
 
 public class AddBalloonActivity extends AppCompatActivity {
     private final static int getImage = 1;
-    private ImageView getImageView;
+   // private ImageView getImageView;
+    private EditBalloonView getImageView;
+    private ArrayList<TextBalloon> mBalloon = new ArrayList<TextBalloon>();
+    private FrameLayout layout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addballoon);
-        setCameraActionBar();
-        Button homeBtm = (Button)findViewById(R.id.homeBtm);
+        setActionBar();
+        layout = (FrameLayout)findViewById(R.id.BalloonLayout);
+       // Button homeBtm = (Button)findViewById(R.id.homeBtm);
+        /*
         homeBtm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+        */
         Log.d("풍선 인텐트","인텐트 시작");
-        getImageView = (ImageView)findViewById(R.id.getimageView);
+       // getImageView = (ImageView)findViewById(R.id.getimageView);
+        getImageView = (EditBalloonView)findViewById(R.id.getimageView);
         Intent intent = new Intent(AddBalloonActivity.this,EditPotoGallery.class);
         Log.d("풍선 인텐트","인텐트 시작");
         startActivityForResult(intent,getImage);
     }
-    private void setCameraActionBar()
+
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.homeBtm:
+                finish();
+                break;
+            case R.id.addBalloonBtm:
+                //mBalloon.add(new TextBalloon(this));
+                TextBalloon t = new TextBalloon(this);
+                layout.addView(t);
+               // layout.addView(t,new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT));
+                Log.d("풍선버튼","눌림"+t.getWidth()+"  "+t.getHeight());
+                break;
+        }
+    }
+    private void setActionBar()
     {
         ActionBar actionBar = getSupportActionBar();      //제공되는 액션바를 가져온다.
 
@@ -52,7 +91,7 @@ public class AddBalloonActivity extends AppCompatActivity {
         actionBar.setDisplayShowTitleEnabled(false);
 
         //LayoutInflater 는 XML에 정의된 리소스들을 뷰의 형태로 변환한다.
-        View mCustomView = LayoutInflater.from(this).inflate(R.layout.back_actionbar,null);       //만들어둔 xml을 뷰로 변경
+        View mCustomView = LayoutInflater.from(this).inflate(R.layout.balloon_actionbar,null);       //만들어둔 xml을 뷰로 변경
         actionBar.setCustomView(mCustomView);       //뷰를 적용
 
         Toolbar parent = (Toolbar)mCustomView.getParent();
@@ -70,6 +109,7 @@ public class AddBalloonActivity extends AppCompatActivity {
                 String mPath = (String)data.getExtras().get("bm");
                 Bitmap bm = BitmapFactory.decodeFile(mPath);
                 getImageView.setImageBitmap(bm);
+
             }
             else{
                 Log.d("풍선 인텐트","코드가 달라?");
@@ -80,4 +120,6 @@ public class AddBalloonActivity extends AppCompatActivity {
             finish();
         }
     }
+
+
 }
